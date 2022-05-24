bcaeb86ae1605ad95f6f6ba3525ac2decdb59b93
henry.fleitas <henry.fleitas@upr.edu.cu>
2022-05-24 16:50:22 -0400

    start

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { Header } from './components/Header';
+import { Lista } from './components/Lista';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Lista />
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/Lista.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./Lista.css"
+
+export function Lista() {
+    return (
+        <div className='home'>
+            <div></div>
+            <div className="box">
+                <div className='user'>
+                    <div className="user-data">
+                        <div className="user-img">Imagen</div>
+                        <div className="user-perfil">Datos</div>
+                    </div>
+                    <div className="user-lang">Lenguajes</div>
+                </div>
+            </div>
+            <div></div>
+
+        </div>
+    )
+}
