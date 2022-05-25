3ecf13ae482457033b8272caae093588df227414
henry.fleitas <henry.fleitas@upr.edu.cu>
2022-05-25 16:44:38 -0400

    add item list

--- src/components/Lista.jsx
+++ src/components/Lista.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./Lista.css"
+import image from "../assets/images/photosnap.svg"
 
 export function Lista() {
     return (
@@ -8,10 +9,30 @@ export function Lista() {
             <div className="box">
                 <div className='user'>
                     <div className="user-data">
-                        <div className="user-img">Imagen</div>
-                        <div className="user-perfil">Datos</div>
+                        <div className="user-img"> <img src={image} alt="logo" /> </div>
+                        <div className="user-perfil">
+                            <ul className='lista-company' >
+                                <li className='company' >Company</li>
+                                <li className='new' >New!</li>
+                                <li className='featured' >Featured</li>
+                            </ul>
+                            {/* <div className="company">tester  test1   test2</div> */}
+                            <div className="position">Senior Fronted Developer</div>
+                            <ul className='user-contract'>
+                                <li>1d ago</li>
+                                <li>Full Time</li>
+                                <li>USA Only</li>
+                            </ul>
+                        </div>
                     </div>
-                    <div className="user-lang">Lenguajes</div>
+                    <div className="user-lang">
+                        <ul className="lang">
+                            <li>Lang1</li>
+                            <li>Lang2</li>
+                            <li>Lang3</li>
+                            <li>Lang4</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
             <div></div>
